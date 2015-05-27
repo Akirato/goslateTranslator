d2179cf272279a48c0e6b41e59510936ff1651aa
Akirato <nurendra.choudhary@research.iiit.ac.in>
2015-05-28 03:18:48 +0530

    analysing.....

--- methodAnalysis/analyse.py
+++ methodAnalysis/analyse.py
@@ -3,16 +3,13 @@ import goslate
 import sys
 gs=goslate.Goslate()
 def phrase_wise(filePath):
-    print(0)
     a=open(filePath,"r").read()
     lines = a.splitlines()
     need = []
     for i in lines:
-        print(1)
         if len(i)>0:
-            if not i[0].isdigit():
+            if not(i[0].isdigit()):
                 need.append(i)
-    print(2)
     return need
 
 def word_wise(filePath):
@@ -20,9 +17,10 @@ def word_wise(filePath):
     lines = a.splitlines()
     need = []
     for i in lines:
-        if not i[0].isdigit():
-            for j in i.split():
-            	need.append(i+" ")
+        if len(i)>0:
+            if not i[0].isdigit():
+            	for j in i.split():
+            	    need.append(i+" ")
     return need
 
 def fullSentence_wise(filePath):
@@ -34,16 +32,27 @@ def fullSentence_wise(filePath):
     return [need]
 
 def translate(need,langid):
-    print(3)
     translated=[]
     for i in need:
         translated.append(gs.translate(i,langid))
     return translated
 
 #print phrase_wise(sys.argv[0])
-fp = open("answer.txt","w")
+fp = open("result_phrase.txt","w")
 for i in phrase_wise(sys.argv[1]):
-    print((translate([i],sys.argv[2])[0].encode('utf-8')),file=fp)
+    if i[0]!='<':
+        print((translate([i],sys.argv[2])[0].encode('utf-8')),file=fp)
 fp.close()
+fp2 = open("result_sentence.txt","w")
+for i in fullSentence_wise(sys.argv[1]):
+    if i[0]!='<':
+        print((translate([i],sys.argv[2])[0].encode('utf-8')),file=fp2)
+fp2.close()
+fp3 = open("result_word.txt","w")
+for i in word_wise(sys.argv[1]):
+    if i[0]!='<':
+        print((translate([i],sys.argv[2])[0].encode('utf-8')),file=fp3)
+fp3.close()
+
 exit()
 
