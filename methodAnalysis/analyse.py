0512be27d3cb5a8fdc78088fa4a4f90293b710c3
Akirato <nurendra.choudhary@research.iiit.ac.in>
2015-05-27 19:33:31 +0530

    more analysis

--- methodAnalysis/analyse.py
+++ methodAnalysis/analyse.py
@@ -1,16 +1,18 @@
+from __future__ import print_function
 import goslate
 import sys
+gs=goslate.Goslate()
 def phrase_wise(filePath):
-    print 0
+    print(0)
     a=open(filePath,"r").read()
     lines = a.splitlines()
     need = []
     for i in lines:
-        print 1
+        print(1)
         if len(i)>0:
             if not i[0].isdigit():
                 need.append(i)
-    print 2
+    print(2)
     return need
 
 def word_wise(filePath):
@@ -32,13 +34,16 @@ def fullSentence_wise(filePath):
     return [need]
 
 def translate(need,langid):
-    print 3
+    print(3)
     translated=[]
     for i in need:
-        translated.append(goslate.Goslate().translate(i,langid))
+        translated.append(gs.translate(i,langid))
     return translated
 
-for i in translate(phrase_wise(sys.argv[0]),'hi'):
-    print i
+#print phrase_wise(sys.argv[0])
+fp = open("answer.txt","w")
+for i in phrase_wise(sys.argv[1]):
+    print((translate([i],sys.argv[2])[0].encode('utf-8')),file=fp)
+fp.close()
 exit()
 
